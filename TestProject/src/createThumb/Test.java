615b81354cb7a4edf7d499f6e9ec20e0cdd07ae5
yosuke.kimura <yosuke.kimura@WD01501195.ad.intra>
2017-04-14 12:32:24 +0900

    aaaa

--- TestProject/src/createThumb/Test.java
+++ TestProject/src/createThumb/Test.java
@@ -6,6 +6,9 @@ import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.mortennobel.imagescaling.AdvancedResizeOp;
 import com.mortennobel.imagescaling.ResampleFilters;
 import com.mortennobel.imagescaling.ResampleOp;
@@ -13,23 +16,23 @@ import com.xuggle.mediatool.IMediaReader;
 import com.xuggle.mediatool.MediaListenerAdapter;
 import com.xuggle.mediatool.ToolFactory;
 import com.xuggle.mediatool.event.IVideoPictureEvent;
+import com.xuggle.xuggler.Global;
 import com.xuggle.xuggler.ICodec;
 import com.xuggle.xuggler.IContainer;
 import com.xuggle.xuggler.IStream;
 import com.xuggle.xuggler.IStreamCoder;
 
 public class Test extends MediaListenerAdapter {
-	private static String INPUT_DIR = "D:\\testtest";
-	private static String OUTPUT_DIR = "D:\\testtest\\ss";
+	private static final String INPUT_DIR = "D:\\testtest";
+	private static final String OUTPUT_DIR = "D:\\testtest\\ss";
 
-	String[] suffixArray = {"wmv","mp4","flv"};
+	private static final String[] SUFFIX_ARRAY = {"wmv","mp4","flv"};
 
-	private int mVideoStreamIndex = -1;
 	File[] files = new File(INPUT_DIR).listFiles();
 	private int fileIndex = 0;
+	private int thumbIndex = 1;
 
-	double timeBase = 0;
-	int videoStreamId = -1;
+	private static final Logger log = LoggerFactory.getLogger(Test.class);
 
 	public static void main(String[] args) {
 		new Test();
@@ -38,46 +41,60 @@ public class Test extends MediaListenerAdapter {
 	public Test() {
 
 		for (File file : files) {
-			if (!Arrays.asList(suffixArray).contains(getSuffix(file.getName()))) {
+			if (!Arrays.asList(SUFFIX_ARRAY).contains(getSuffix(file.getName()))) {
 				fileIndex++;
 				continue;
 			}
 			IMediaReader reader = ToolFactory.makeReader(file.getPath());
 			try {
 				reader.open();
-
-				timeBase = 0;
-				videoStreamId = -1;
-				seekToMs(reader.getContainer(), 10000);
-
 				reader.setBufferedImageTypeToGenerate(5);
-
 				reader.addListener(this);
 
-				while (reader.readPacket() == null && mVideoStreamIndex == -1) {
+				IContainer container = reader.getContainer();
+				if (container.getDuration() == Global.NO_PTS) {
+					continue;
 				}
+				log.info("# Duration (ms): " + container.getDuration() / 100);
+				long thumbBetweenDurationMs = container.getDuration() / 500;
+
+				int videoStreamId = -1;
+				double timeBase = 0;
+		        for(int i = 0; i < container.getNumStreams(); i++) {
+		            IStream stream = container.getStream(i);
+		            IStreamCoder coder = stream.getStreamCoder();
+		            if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO) {
+		                videoStreamId = i;
+		                timeBase = stream.getTimeBase().getDouble();
+		                break;
+		            }
+		        }
+
+				long totalDuration = 0;
+				for (int i = 0; i < 5; i++) {
+					if (i == 0) {
+						seekToMs(container, 10000, videoStreamId, timeBase);
+					} else {
+						seekToMs(container, totalDuration, videoStreamId, timeBase);
+					}
+					totalDuration += thumbBetweenDurationMs;
+					thumbIndex = i + 1;
+					reader.readPacket();
+				}
+
 			} catch(Exception e) {
 				e.printStackTrace();
 			} finally {
+				reader.close();
 			}
-			reader.close();
-			mVideoStreamIndex = -1;
 			fileIndex++;
 		}
 	}
 
 	public void onVideoPicture(IVideoPictureEvent event) {
 		try {
-			if (event.getStreamIndex().intValue() != this.mVideoStreamIndex) {
-				if (-1 == this.mVideoStreamIndex) {
-					this.mVideoStreamIndex = event.getStreamIndex().intValue();
-				} else {
-					return;
-				}
-			}
 			BufferedImage sumbImage = reSize3(event.getImage(), 90,75);
-//			File outputFile = File.createFile("frame", ".png", new File(OUTPUT_DIR));
-			File outputFile = new File(OUTPUT_DIR, "_smb_".concat(files[fileIndex].getName()).concat(".png"));
+			File outputFile = new File(OUTPUT_DIR, "_smb" + thumbIndex + "_" + files[fileIndex].getName() + ".png");
 
 			ImageIO.write(sumbImage, "png", outputFile);
 
@@ -104,21 +121,8 @@ public class Test extends MediaListenerAdapter {
 		return rescaled;
 	}
 
-	private void seekToMs(IContainer container, long timeMs) {
-	    if(videoStreamId == -1) {
-	    	int test = container.getNumStreams();
-	    	IStream stream2 = container.getStream(0);
-	        for(int i = 0; i < container.getNumStreams(); i++) {
-	            IStream stream = container.getStream(i);
-	            IStreamCoder coder = stream.getStreamCoder();
-	            if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO) {
-	                videoStreamId = i;
-	                timeBase = stream.getTimeBase().getDouble();
-	                break;
-	            }
-	        }
-	    }
-
+	private void seekToMs(IContainer container, long timeMs, int videoStreamId, double timeBase) {
+		log.info("# Seek (ms): " + timeMs);
 	    long seekTo = (long) (timeMs/1000.0/timeBase);
 	    container.seekKeyFrame(videoStreamId, seekTo, IContainer.SEEK_FLAG_BACKWARDS);
 	}
